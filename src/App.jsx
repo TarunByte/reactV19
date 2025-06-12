95655ed61a608cdd256018217af11775df7c0c94
TarunBits <tarunbhattmortal@gmail.com>
2025-06-12 21:40:41 +0530

    State App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-// import NetflixSeries from "./components/NetflixSeries";
-// import "./components/Netflix.css";
-import "./components/Netflix.module.css";
-// import { EventHandling } from "./components/EventHandling";
-// import { EventProps } from "./components/EventProps";
-import { EventPropagation } from "./components/EventPropagation";
+import "./components/EV.css";
+import { State } from "./components/hooks/State";
 
 export const App = () => {
   return (
     <section className="container">
-      {/* <h1 className="card-heading">List of Best Netflix Series</h1> */}
-      {/* <NetflixSeries /> */}
-      {/* <EventHandling /> */}
-      {/* <EventProps /> */}
-      <EventPropagation />
+      <State />
     </section>
   );
 };
