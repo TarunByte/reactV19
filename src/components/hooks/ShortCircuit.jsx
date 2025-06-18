529fb2bb91819f391f9182ae4233f4bc6382bee4
TarunBits <tarunbhattmortal@gmail.com>
2025-06-18 17:45:03 +0530

    #44 React Hook Rules!

--- src/components/hooks/ShortCircuit.jsx
+++ src/components/hooks/ShortCircuit.jsx
@@ -1,10 +1,22 @@
 // import "./Hooks.css";
-
 import { useState } from "react";
 
 const ShortCircuitExample = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [user, setUser] = useState("");
+  // const [count, setCount] = useState(0);
+
+  // if (user) {
+  //   const [count, setCount] = useState(0);
+  //   // setCount(count + 1);
+  // }
+
+  // const getData = () => {
+  //   const [data, setData] = useState(0);
+  // };
+
+  // getData();
+
   return (
     <section className="container short-container">
       <h1>Welcome to the ShortCircuit Evaluation!</h1>
