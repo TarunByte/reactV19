79439bff0ba0a7a44369c41233c5496bbde80ed4
TarunBits <tarunbhattmortal@gmail.com>
2025-06-23 12:31:32 +0530

    #56 Fetching API Data in React

--- src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -1,25 +1,52 @@
-// import "./project/pokemon.css";
+import "./pokemon.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const HowNotToFetchApi = () => {
-  const [apiData, setApiData] = useState([]);
+  const [pokemon, setpokemon] = useState(null);
 
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-      // setApiData(data);
-    })
-    .catch((error) => console.log(error));
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
+  const fetchPokemon = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setpokemon(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  console.log(pokemon);
+
+  if (!pokemon)
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+
+  // if (pokemon) {
   return (
-    <div className="container effect-container">
-      <ul>
-        data:
-        {apiData.map((curData) => {
-          return <li key={curData.id}> {curData.title}</li>;
-        })}
+    <section className="container">
+      <header>
+        <h1>Lets Catch Pokemon</h1>
+      </header>
+      <ul className="card-demo">
+        <li className="pokemon-card">
+          <figure>
+            <img
+              src={pokemon.sprites.other.dream_world.front_default}
+              alt={pokemon.name}
+              className="pokemon-image"
+            />
+          </figure>
+          <h1>{pokemon.name}</h1>
+        </li>
       </ul>
-    </div>
+    </section>
   );
 };
