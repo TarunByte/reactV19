ab83793c6f276f46649e2be51ebbb1e671865df7
TarunBits <tarunbhattmortal@gmail.com>
2025-06-27 13:47:44 +0530

    #64 Prop Drilling in React 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import { UseRef } from "./components/hooks/useRefs";
-import { UseId } from "./components/hooks/UseID";
+// import { UseId } from "./components/hooks/UseID";
+import { ParentComponent } from "./components/PropDrilling";
 
 export const App = () => {
-  return <UseId />;
+  return <ParentComponent />;
 };
